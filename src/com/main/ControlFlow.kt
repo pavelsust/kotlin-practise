3bde89723ccc854b0ddd5ccd7a7e8967cd84ed9b
paveltech <pavelcep.sust11@gmail.com>
2019-05-31 11:48:22 +0600

    while added

--- src/com/main/ControlFlow.kt
+++ src/com/main/ControlFlow.kt
@@ -9,7 +9,6 @@ fun main(args: Array<String>){
     var find = if (number_1>number_2) number_1 else number_2;
     println(find);
 
-
     /// if branches can be block
 
     val max = if (number_1>number_2){
@@ -22,11 +21,33 @@ fun main(args: Array<String>){
 
     println(max)
 
-    var x = 3;
+    val x = 4;
 
     when(x){
-        1 -> println("it is 1")
-        2 -> println("it is 2")
-        3 -> println("it is 3")
+        2 -> println("found 2")
+        3 -> println("found 3")
+        4 -> println("found 4")
+        else -> println("not found")
+    }
+
+
+    /**
+     *  we can also check the value from a range
+     */
+
+    var y = 30;
+
+    when(y){
+        in 1..20 -> println("found");
+        !in 1..20 -> println("out side the range")
+        else -> println("not found")
+    }
+
+    /**
+     *  check directly number is odd or even from when
+     */
+
+    when(y){
+
     }
 }
